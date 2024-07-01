3059ef184d09d534e2fccd845d960eb2dde1d030
230010 <230010@E0404-16>
2024-07-01 15:10:32 +0900

    Enemy

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,1 +1,24 @@
 #include "Enemy.h"
+#include <assert.h>
+#include "Camera.h"
+#include "TestScene.h"
+#include"EMP.h"
+
+namespace {
+	static const int SCREEN_WIDTH = 1280;
+};
+
+Enemy::Enemy(GameObject* scene)
+{
+	hImage = LoadGraph("Assets/mob.png");
+	assert(hImage > 0);
+	transform_.position_.x = 800.0f;
+	transform_.position_.y = 500.0f;
+}
+Enemy::~Enemy()
+{
+	if (hImage > 0)
+	{
+		DeleteGraph(hImage);
+	}
+}
--- Enemy.h
+++ Enemy.h
@@ -1,5 +1,29 @@
 #pragma once
-class Enemy
+#include "Engine/GameObject.h"
+
+/// <summary>
+/// ロボット（敵）
+/// </summary>
+class Enemy : public GameObject
 {
+public:
+	Enemy(GameObject* scene);
+	~Enemy();
+	void Update() override;
+	void Draw() override;
+
+	/// <summary>
+	/// ロボットの座標をセットする
+	/// </summary>
+	/// <param name="x">X座標</param>
+	/// <param name="y">Y座標</param>
+	void SetPosition(int x, int y);
+
+	//円の当たり判定をする
+	bool CollideCircle(float x, float y, float r);
+private:
+	float sinAngle;
+	int hImage;
+	float baseY;//生成時のY座標
 };
 
